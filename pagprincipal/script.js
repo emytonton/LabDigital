084cb32962909ed23f85b2e22e901b2f94f97b61
Emilly Paiva <emillypaiva3260@gmail.com>
2025-02-11 16:15:00 -0300

    :sparkles: feat: Página de login

---
+++ pagprincipal/script.js
@@ -0,0 +1,25 @@
+const profileLink = document.getElementById('profilenav');
+        const profileDropdown = document.getElementById('profileDropdown');
+
+        profileLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            profileDropdown.classList.toggle('show');
+        });
+
+        document.getElementById('logout').addEventListener('click', function() {
+            alert('Você foi deslogado!');
+            //adicionar logica para deslogar
+        });
+
+        window.onclick = function(event) {
+            if (!event.target.matches('#profilenav')) {
+                if (profileDropdown.classList.contains('show')) {
+                    profileDropdown.classList.remove('show');
+                }
+            }
+        }
+
+document.getElementById("logout").addEventListener("click", function() {
+            window.location.href = "../login/index.html"; 
+        });
+        
